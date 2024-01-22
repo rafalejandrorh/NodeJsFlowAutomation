6814ca90d44be38f35457b0fba1dec5076acb7c6
Rafael Rivero <rafalejandrorivero@gmail.com>
2024-01-22 19:58:39 -0400

    Fix (Bot): Variable Conflict in mail class

--- src/bot/services/mail.service.js
+++ src/bot/services/mail.service.js
@@ -7,11 +7,11 @@ class MailService {
     constructor() {
     }
 
-    async sendMail(email, data) {
+    async sendMail(email, dataBody) {
 
         const { data } = await axios.post(`${endpointV1}/mail/${email}/send`, {
-            subject: data.subject,
-            html: data.html
+            subject: dataBody.subject,
+            html: dataBody.html
         });
         console.log('sendMail: ', data);
         
