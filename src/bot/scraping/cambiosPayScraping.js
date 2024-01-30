850f7e94a9d7b316c9bd09658290473f6101dbff
Rafael Rivero <rafalejandrorivero@gmail.com>
2024-01-29 20:32:15 -0400

    Refactor (Bot): cambiosPayScraping.js

--- src/bot/scraping/cambiosPayScraping.js
+++ src/bot/scraping/cambiosPayScraping.js
@@ -16,17 +16,15 @@ async function getDollarPrice() {
         const getDollarPrice = await page.evaluate(() => {
             const elements = {};
     
-
             const dollarClass = document.querySelector('span.span.tasa_pp');
             console.log('dollarClass: ', dollarClass);
             elements.dolar = dollarClass.innerHTML;
     
-            //return {dolar:'40,48540000',euro:'42,71173256',rublo:'1,40015471'}
             return elements;
         });
         getDollarPrice.fecha = time.format('YYYY-MM-DD HH:mm:ss');
         console.log('cambiosPayScraping.getDollarPrice: ', getDollarPrice);
-
+    
         await browser.close();
         return getDollarPrice;
     } catch (error) {
