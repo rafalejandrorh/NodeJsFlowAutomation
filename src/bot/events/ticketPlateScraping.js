05e8049c3e9dc9d953885355e10331ae7699975c
Rafael Rivero <rafalejandrorivero@gmail.com>
2024-02-03 15:06:08 -0400

    Refactor (Bot): modify files related to TicketPlate

--- src/bot/events/ticketPlateScraping.js
+++ src/bot/events/ticketPlateScraping.js
@@ -1,21 +1,16 @@
 const { telegram: { bot: { replySettingsDefault } } } = require('../../../config');
 
 const EventEmitter = require('events');
-const webScrapingEvents = new EventEmitter();
-
-const ticketPlateScraping = require('../scraping/ticketPlateScraping');
+const ticketPlateEvents = new EventEmitter();
 
 const TicketPlateService = require('../services/ticketPlate.service');
 const ticketPlateService = new TicketPlateService();
 
-const MailService = require('../services/mail.service');
-const mailService = new MailService();
-
-webScrapingEvents.on('validatePreOlympicTicketAvailability', async (bot) => {
+ticketPlateEvents.on('validatePreOlympicTicketAvailability', async (bot) => {
     const reply = await ticketPlateService.validatePreOlympicTicketAvailability();
     
     // Enviar mensaje a usuarios específicos de Telegram (Utilizar para enviar precio del dólar)
     bot.telegram.sendMessage(1026291175, reply, replySettingsDefault);
 });
 
-module.exports = webScrapingEvents;
+module.exports = ticketPlateEvents;
--- src/bot/scraping/ticketPlateScraping.js
+++ src/bot/scraping/ticketPlateScraping.js
@@ -2,10 +2,6 @@ const puppeteer = require('puppeteer');
 
 const { regExp: { conmebolPreOlympicTicket: regExpConmebolPreOlympicTicket } } = require('../../../config') 
 
-const regexNgContent = /_ngcontent-ng-/;
-const regexAgotado = /AGOTADO/;
-const regexOneNumber = /\d+/;
-
 async function validatePreOlympicTicketAvailability() {
     try {
         let preOlympicTickets = [];
@@ -17,21 +13,10 @@ async function validatePreOlympicTicketAvailability() {
         const page = await browser.newPage();
         await page.goto('https://ticketplate.com/');
 
-        // const PreOlympicTicket = await page.evaluate(() => {
-        //     const elements = {};
-    
-        //     const eventsContainerDiv = document.querySelector('.events-container');
-        //     console.log('eventsContainerDiv: ', eventsContainerDiv);
-
-        //     elements.dolar = dollarClass.innerHTML;
-    
-        //     return elements;
-        // });
         const h5Elements = await page.$$('h5');
 
-        // Recorremos cada elemento h5
         for (let h5 of h5Elements) {
-            // Obtenemos el texto y atributos del elemento
+
             const elements = await page.evaluate(element => {
                 return {
                     text: element.innerText,
@@ -39,31 +24,13 @@ async function validatePreOlympicTicketAvailability() {
                 };
             }, h5);
 
-            // Comprobamos si el texto coincide con la expresión regular
             if (regExpConmebolPreOlympicTicket.test(elements.text)) {
-                // Imprimimos el texto por la consola
                 console.log(elements.text);
                 preOlympicTickets.push(elements.text);
-                // for (let attribute of elements.attributes) {
-                //     if(regexNgContent.test(attribute)) {
-                //         // Imprimimos el atributo por la consola
-                //         console.log(attribute);
-
-                //         const pElements = await page.$$(`p[${attribute}]`);
-                //         for (let p of pElements) {
-                //             const date = await page.evaluate(element => element.innerText, p);
-                //             //if(regexOneNumber.test(date)) {
-                //                 console.log(date);
-                //             //}
-                //         }
-
-                //     }
-                // }
-    
             }
         }
-    
         await browser.close();
+        
         return preOlympicTickets;
     } catch (error) {
         console.log(error);
